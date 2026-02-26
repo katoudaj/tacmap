58fc9121638a30cd5f001e8e7e5106fda519833e
katoudaj <katoudaj@gmail.com>
2026-02-27 00:56:05 +0900

    heads川越のマップを追加

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,6 @@ export const PIN_MAX_DURATION = 30; // ピンが消えるまでの秒数
 
 export const availableMaps = [
     { name: "BLKFOX 福生店", src: "/maps/blkfox.png" },
+    { name: "H.E.A.D.S 川越", src: "/maps/heads川越.jpg" },
     //{ name: "デバッグ用", src: "/maps/debug.png" },
 ];
