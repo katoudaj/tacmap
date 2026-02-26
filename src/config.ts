212215bf9da7af9c123a4be90bb8933eee0354f9
katoudaj <katoudaj@gmail.com>
2026-02-27 00:59:19 +0900

    blkfox福生店のマップ差し替え

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const PIN_MAX_DURATION = 30; // ピンが消えるまでの秒数
 
 export const availableMaps = [
-    { name: "BLKFOX 福生店", src: "/maps/blkfox.png" },
+    { name: "BLKFOX 福生店", src: "/maps/blkfox福生店.png" },
     { name: "H.E.A.D.S 川越", src: "/maps/heads川越.jpg" },
     //{ name: "デバッグ用", src: "/maps/debug.png" },
 ];
